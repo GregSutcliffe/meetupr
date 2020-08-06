835a555bfd7c63d16bc87bad2c49cb4f0bb7dd0d
Greg Sutcliffe <github@emeraldreverie.org>
2020-08-06 11:58:17 +0100

    Add `joined` field as per docs

--- R/get_members.R
+++ R/get_members.R
@@ -32,6 +32,7 @@ get_members <- function(urlname, api_key = NULL){
     name = purrr::map_chr(res, "name", .default = NA),  
     bio = purrr::map_chr(res, "bio", .default = NA),
     status = purrr::map_chr(res, "status"),
+    joined = .date_helper(purrr::map_chr(res, "joined")),
     created = .date_helper(purrr::map_dbl(res, c("group_profile", "created"))),
     city = purrr::map_chr(res, "city", .default = NA),
     country = purrr::map_chr(res, "country", .default = NA),
